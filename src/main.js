0a49d80b44e42eff6225c91f538e60b764722f23
chenliang <ygglyang@gmail.com>
2022-04-29 09:11:53 +0800

    加通知栏

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Buefy)
+Vue.use(ElementUi)
 
 Vue.config.productionTip = false
 
