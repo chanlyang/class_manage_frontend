9156faead6075c093c9a25eaac0028eab957d21e
chenliang <ygglyang@gmail.com>
2022-05-02 00:35:55 +0800

    加一下api请求方法

--- src/api/anti.js
+++ src/api/anti.js
@@ -1,1 +1,45 @@
 import request from '@/utils/request'
+
+//每日学习的反诈信息
+export function info() {
+    return request({
+        url: '/api/fraud/info',
+        method: 'get',
+    })
+}
+
+//反诈信息打卡
+export function punch(anitId) {
+    return request({
+        url: '/api/fraud/',
+        method: 'get',
+        params: { anitId: anitId }
+    })
+}
+
+//发布反诈信息
+export function publish(data) {
+    return request({
+        url: '/api/fraud/publish',
+        method: 'post',
+        data: data
+    })
+}
+
+//反诈信息列表
+export function pageList(pageNo, size) {
+    return request({
+        url: '/api/fraud/pageList',
+        method: 'get',
+        params: { pageNo: pageNo, size: size }
+    })
+}
+
+//学生打卡列表
+export function studentPunch(pageNo, size, anitId) {
+    return request({
+        url: '/api/fraud/studentPunch',
+        method: 'get',
+        params: { pageNo: pageNo, size: size, anitId: anitId }
+    })
+}
--- src/api/student.js
+++ src/api/student.js
@@ -1,1 +1,10 @@
 import request from '@/utils/request'
+
+//用户接受公告列表
+export function pageList(pageNo, size) {
+    return request({
+        url: '/api/student/pageList',
+        method: 'get',
+        params: { pageNo: pageNo, size: size }
+    })
+}
